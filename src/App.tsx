29b9ce1b198d4df8cac6107601cdd66893320b16
Mohammad Shabeer <moshdev2213@gmail.com>
2024-11-21 11:27:36 +0530

    mod:logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import gcr from './assets/gcr.png'
+import gar from './assets/gar.png'
+import gcs from './assets/gcs.png'
+import terraform_logo from './assets/terraform_logo.png'
+
 import './App.css'
 
 function App() {
@@ -8,25 +12,37 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className='align_symbols'>
+        <a href="" target="_blank">
+          <img src={gcr} className="logo" alt="Vite logo" />
+        </a>
+        <a href="" target="_blank">
+          <img src={gcs} className="logo" alt="Vite logo" />
+        </a>
+        <a href="" target="_blank">
+          <img src={gar} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <div className='align_symbols'>
+        <a href="" target="_blank">
+          <img src={terraform_logo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h2>GCR + GAR + GCS + Terraform</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click Me : {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Created In Order To Learn GCP Deployments
+      </p>
+      <p className="read-the-docs">
+        made with ❤ <a target='_blank' href="https://github.com/moshdev2213">@moshdev2213</a>
       </p>
     </>
   )
